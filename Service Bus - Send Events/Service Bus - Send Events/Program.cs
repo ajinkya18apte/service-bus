a8065cce038ddecb2bd1ba3d809a6fa46293ba9f
Ajinkya Apte <Ajinkya.Apte@cloudmoyo.com>
2020-02-12 15:16:01 +0530

    Commit

---
+++ Service Bus - Send Events/Service Bus - Send Events/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+
+namespace Service_Bus___Send_Events
+{
+    class Program
+    {
+        const string ServiceBusConnectionString = "Endpoint=sb://cm-poc-sbus-alias.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=uZl1WOE5CKWsphW2PDfey2cLryJY5AkWKS/XxPqhx1g=";
+        const string QueueName = "cm-poc-sbus-q-001";
+        static IQueueClient queueClient;
+        public static async Task Main(string[] args)
+        {
+            const int numberOfMessages = 10;
+            queueClient = new QueueClient(ServiceBusConnectionString, QueueName);
+
+            Console.WriteLine("======================================================");
+            Console.WriteLine("Press ENTER key to exit after sending all the messages.");
+            Console.WriteLine("======================================================");
+
+            // Send messages.
+            await SendMessagesAsync(numberOfMessages);
+
+            Console.ReadKey();
+
+            await queueClient.CloseAsync();
+        }
+        static async Task SendMessagesAsync(int numberOfMessagesToSend)
+        {
+            try
+            {
+                for (var i = 10; i < 20; i++)
+                {
+                    // Create a new message to send to the queue.
+                    string messageBody = $"Message {i}";
+                    var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+
+                    // Write the body of the message to the console.
+                    Console.WriteLine($"Sending message: {messageBody}");
+
+                    // Send the message to the queue.
+                    await queueClient.SendAsync(message);
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
+            }
+        }
+    }
+}
